76c28dc1e7096eb21c081be41c565a716bb6a849
Kota Uchida <uchan0@gmail.com>
2012-07-12 15:53:20 +0900

    コメント追加

--- src/com/github/uchan_nos/c_helper/analysis/ReachingDefinition.java
+++ src/com/github/uchan_nos/c_helper/analysis/ReachingDefinition.java
@@ -3,11 +3,15 @@ package com.github.uchan_nos.c_helper.analysis;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.BitSet;
-import java.util.NoSuchElementException;
 
 import org.eclipse.cdt.core.dom.ast.*;
 
+/**
+ * 到達定義解析を行うクラス.
+ * @author uchan
+ */
 public class ReachingDefinition {
+    // gen, kill集合を保持する構造体
     private static class GenKill {
         final public BitSet gen;
         final public BitSet kill;
@@ -17,27 +21,40 @@ public class ReachingDefinition {
         }
     }
 
-    private CFG cfg;
-    private ArrayList<AssignExpression> assignList;
+    private CFG cfg; // 解析すべきソースのフローグラフ
+    private ArrayList<AssignExpression> assignList; // cfgに含まれる代入文のリスト
+
+    /**
+     * 到達定義実行器を生成する.
+     * @param cfg 解析するソースコードの制御フローグラフ
+     * @param assignList ソースコード中の代入文のリスト
+     */
     public ReachingDefinition(CFG cfg, ArrayList<AssignExpression> assignList) {
         this.cfg = cfg;
         this.assignList = assignList;
     }
 
     public void analyze() {
+        // フローグラフ中の頂点数
         final int numVertex = cfg.getVertices().size();
-        ArrayList<GenKill> genkill = new ArrayList<ReachingDefinition.GenKill>(numVertex);
+
         ArrayList<CFG.Vertex> vertices = new ArrayList<CFG.Vertex>(CFGPrinter.sort(cfg.getVertices()));
+
+        // 各頂点に対応するgen, killを生成
+        ArrayList<GenKill> genkill = new ArrayList<ReachingDefinition.GenKill>(numVertex);
         for (int i = 0; i < numVertex; ++i) {
             genkill.add(createGenKill(vertices.get(i)));
         }
 
+        // 各頂点の入口と出口の到達定義集合
         ArrayList<BitSet> entry = new ArrayList<BitSet>(numVertex);
         ArrayList<BitSet> exit = new ArrayList<BitSet>(numVertex);
-        ArrayList<BitSet> exitPrev = null;
+        // exitに変更が加わったか調べるために過去の値を保存しておく
+        ArrayList<BitSet> exitPrev = new ArrayList<BitSet>(numVertex);
         for (int i = 0; i < numVertex; ++i) {
             entry.add(new BitSet(assignList.size()));
             exit.add(new BitSet(assignList.size()));
+            exitPrev.add(new BitSet(assignList.size()));
         }
 
         boolean modified = true;
@@ -47,7 +64,7 @@ public class ReachingDefinition {
                     int leadingIndex = vertices.indexOf(leading);
                     entry.get(i).or(exit.get(leadingIndex));
                 }
-                exit.get(i).clear();
+                // exit, entryは単調増加であるので、clearしなくてよい
                 exit.get(i).or(entry.get(i));
                 exit.get(i).andNot(genkill.get(i).kill);
                 exit.get(i).or(genkill.get(i).gen);
@@ -55,7 +72,10 @@ public class ReachingDefinition {
             if (exit.equals(exitPrev)) {
                 modified = false;
             } else {
-                exitPrev = (ArrayList<BitSet>)exit.clone();
+                for (int i = 0; i < exit.size(); ++i) {
+                    // exit, entryは単調増加であるので、clearしなくてよい
+                    exitPrev.get(i).or(exit.get(i));
+                }
             }
         }
 
@@ -80,20 +100,30 @@ public class ReachingDefinition {
         }
     }
 
+    /**
+     * 指定された頂点のgen, kill集合を生成する.
+     * @param v gen, kill集合を生成したい頂点
+     * @return gen, kill集合
+     */
     private GenKill createGenKill(CFG.Vertex v) {
         BitSet kill = new BitSet(assignList.size());
         BitSet gen = new BitSet(assignList.size());
         for (int i = v.getASTNodes().size() - 1; i >= 0; --i) {
             GenKill sub = createGenKill(v.getASTNodes().get(i));
             if (sub != null) {
-                sub.gen.andNot(kill);
-                gen.or(sub.gen);
-                kill.or(sub.kill);
+                if (sub.gen != null) {
+                    sub.gen.andNot(kill);
+                    gen.or(sub.gen);
+                }
+                if (sub.kill != null) {
+                    kill.or(sub.kill);
+                }
             }
         }
         return new GenKill(gen, kill);
     }
 
+    // 指定されたASTノードのgen, kill集合を生成する.
     private GenKill createGenKill(IASTNode ast) {
         if (ast instanceof IASTExpressionStatement) {
             return createGenKill((IASTExpressionStatement)ast);
@@ -104,6 +134,7 @@ public class ReachingDefinition {
     }
 
     private GenKill createGenKill(IASTExpressionStatement ast) {
+        // 代入に対応したgen, kill
         AssignExpression assign = getAssignExpressionOfNode(ast.getExpression());
         ArrayList<AssignExpression> killedAssigns = null;
 
@@ -128,21 +159,31 @@ public class ReachingDefinition {
     }
 
     private GenKill createGenKill(IASTDeclarationStatement ast) {
+        // 初期化付き変数定義に対応したgen, kill
         if (ast.getDeclaration() instanceof IASTSimpleDeclaration) {
             IASTDeclarator[] declarators =
                     ((IASTSimpleDeclaration)ast.getDeclaration()).getDeclarators();
             BitSet gen = new BitSet(assignList.size());
             for (int i = 0; i < declarators.length; ++i) {
-                AssignExpression assign = getAssignExpressionOfNode(declarators[i]);
-                if (assign != null) {
+                if (declarators[i].getInitializer() != null) {
+                    AssignExpression assign = getAssignExpressionOfNode(declarators[i]);
+                    // assignがnullになるのは、初期化なし変数定義の場合
                     gen.set(assign.getId());
                 }
             }
-            return new GenKill(gen, new BitSet());
+            // 変数定義の場合、定義された変数は他の変数をkillしない
+            return new GenKill(gen, null);
         }
         return null;
     }
 
+    /**
+     * 指定されたASTノードに対応する変数定義を取得する.
+     * 指定するASTノードは初期化付き変数定義のIASTDeclaratorか
+     * 代入文のIASTBinaryExpressionに対応している.
+     * @param ast
+     * @return
+     */
     private AssignExpression getAssignExpressionOfNode(IASTNode ast) {
         for (int i = 0; i < assignList.size(); ++i) {
             if (assignList.get(i).getAST() == ast) {
@@ -152,6 +193,11 @@ public class ReachingDefinition {
         return null;
     }
 
+    /**
+     * 指定された名前を左辺値として持つ変数定義の一覧を取得する.
+     * @param name 抽出する名前
+     * @return 抽出された変数定義一覧
+     */
     private ArrayList<AssignExpression> getAssignExpressionsOfName(IASTName name) {
         ArrayList<AssignExpression> result = new ArrayList<AssignExpression>();
         for (int i = 0; i < assignList.size(); ++i) {
