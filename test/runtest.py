66bf12c2c6c1e030666f7a787fdc87d9bade403b
Kota Uchida <uchan0@gmail.com>
2012-07-12 10:22:44 +0900

    自動テストスクリプトを改良

--- test/runtest.py
+++ test/runtest.py
@@ -1,19 +1,41 @@
 import os
 import re
+import subprocess
 
 if __name__ == '__main__':
 	test_input_dir = 'input'
 	test_output_dir = 'output'
 	test_result_dir = 'result'
-	testcase_name = re.compile(r'(.+).in')
+	testcase_name = re.compile(r'(.+).c')
 	input_files = os.listdir(test_input_dir)
 	output_files = os.listdir(test_output_dir)
+	test_passed = 0
+	numtest = 0
 	for f in input_files:
 		m = testcase_name.match(f)
-		if m:
+		if m and len(m.group(0)) == len(f):
+			numtest += 1
 			name = m.group(1)
-			if name + '.out' not in output_files:
-				print('cannot find output file ' name + '.out')
+			print('------------')
+			print('running test "' + name + '"')
+			print('------------')
+			if name + '.dot' not in output_files:
+				print('cannot find output file ' + name + '.dot')
 			else:
+				cmd = 'CLASSPATH=../bin:`python ./searchplugins.py` '+ \
+						'java com.github.uchan_nos.c_helper.Launcher ' + \
+						'input/' + name + '.c' + \
+						' | diff -u output/' + name + '.dot -'
+				p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
+				(stdoutdata, stderrdata) = p.communicate()
+				if stdoutdata != None and len(stdoutdata) <= 1:
+					print('PASS')
+					test_passed += 1
+				elif stdoutdata != None and len(stdoutdata) > 1:
+					print stdoutdata,
+				else:
+					print('stdout is null')
+	print('-------------------------')
+	print('Result: passed = %d / %d' % (test_passed, numtest))
 
 
