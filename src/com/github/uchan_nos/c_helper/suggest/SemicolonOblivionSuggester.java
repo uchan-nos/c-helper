7996c2ead27de3630513dc3eae50e0bcd223be31
Kota Uchida <uchan0@gmail.com>
2012-08-21 09:22:05 +0900

    セミコロン忘れ警告のバグ取り（構造体型宣言と同時に変数定義をするような場合にも対応）

--- src/com/github/uchan_nos/c_helper/suggest/SemicolonOblivionSuggester.java
+++ src/com/github/uchan_nos/c_helper/suggest/SemicolonOblivionSuggester.java
@@ -6,16 +6,13 @@ import java.util.Collection;
 import org.eclipse.cdt.core.dom.ast.IASTCompositeTypeSpecifier;
 import org.eclipse.cdt.core.dom.ast.IASTDeclSpecifier;
 import org.eclipse.cdt.core.dom.ast.IASTDeclaration;
-import org.eclipse.cdt.core.dom.ast.IASTFunctionDefinition;
-import org.eclipse.cdt.core.dom.ast.IASTNode;
+import org.eclipse.cdt.core.dom.ast.IASTSimpleDeclaration;
 import org.eclipse.cdt.core.dom.ast.IASTTranslationUnit;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IDocument;
-import org.eclipse.jface.text.IRegion;
 
 import com.github.uchan_nos.c_helper.util.DoNothingASTVisitor;
 import com.github.uchan_nos.c_helper.util.Util;
-import com.github.uchan_nos.c_helper.util.Util.CharPredicate;
 
 public class SemicolonOblivionSuggester extends Suggester {
 
@@ -26,6 +23,9 @@ public class SemicolonOblivionSuggester extends Suggester {
                 new ArrayList<Suggestion>();
 
         class Visitor extends DoNothingASTVisitor {
+            private boolean parentIsSimpleDeclaration = false;
+            private int semicolonOffset = -1;
+
             @Override
             public int visit(IASTTranslationUnit tu) {
                 for (IASTDeclaration decl : tu.getDeclarations()) {
@@ -38,54 +38,39 @@ public class SemicolonOblivionSuggester extends Suggester {
 
             @Override
             public int visit(IASTDeclaration declaration) {
-                if (!(declaration instanceof IASTFunctionDefinition)) {
-                    for (IASTNode node : declaration.getChildren()) {
-                        node.accept(this);
+                if (declaration instanceof IASTSimpleDeclaration) {
+                    IASTSimpleDeclaration sd = (IASTSimpleDeclaration) declaration;
+                    parentIsSimpleDeclaration = true;
+
+                    try {
+                        if (input.getSource().getChar(
+                                sd.getFileLocation().getNodeOffset()
+                                + sd.getFileLocation().getNodeLength() - 1) == ';') {
+                            semicolonOffset = sd.getFileLocation().getNodeOffset()
+                                    + sd.getFileLocation().getNodeLength() - 1;
+                        }
                     }
+                    catch (BadLocationException e) {
+                        assert false : "must not be here";
+                        e.printStackTrace();
+                    }
+
+                    sd.getDeclSpecifier().accept(this);
+                    parentIsSimpleDeclaration = false;
+                    semicolonOffset = -1;
                 }
                 return super.visit(declaration);
             }
 
             @Override
             public int visit(IASTDeclSpecifier declSpec) {
-                class WhitespaceCharPredicate implements CharPredicate {
-                    private final char key;
-                    public WhitespaceCharPredicate(char key) {
-                        this.key = key;
-                    }
-                    @Override
-                    public boolean evaluate(char c) {
-                        return !Character.isWhitespace(c) || key == c;
-                    }
-                }
-                final CharPredicate semicolonPredicate = new WhitespaceCharPredicate(';');
-
                 if (declSpec instanceof IASTCompositeTypeSpecifier) {
                     // 構造体に対する declaration specifier は閉じカッコ}までを指す
                     try {
                         final IDocument src = input.getSource();
                         final int endingLine = declSpec.getFileLocation().getEndingLineNumber() - 1;
-                        final int endingOffset = declSpec.getFileLocation().getNodeOffset() + declSpec.getFileLocation().getNodeLength();
 
-                        boolean foundSemicolon = false;
-                        for (int line = 0; line < 2; ++line) {
-                            final IRegion lineInfo = src.getLineInformation(endingLine + line);
-                            final String lineString =
-                                    line == 0 ? src.get(endingOffset, lineInfo.getOffset() + lineInfo.getLength() - endingOffset)
-                                            : src.get(lineInfo.getOffset(), lineInfo.getLength());
-                            final int pos = Util.indexOf(lineString, semicolonPredicate, 0, input.getAst().getComments(), -lineInfo.getOffset());
-                            if (pos >= 0 && lineString.charAt(pos) == ';') {
-                                foundSemicolon = true;
-                                break;
-                            }
-                        }
-
-                        int semicolonOffset = Util.indexOf(src.get(), semicolonPredicate, endingOffset, input.getAst().getComments(), 0);
-                        if (src.get().charAt(semicolonOffset) != ';') {
-                            semicolonOffset = -1;
-                        }
-                        //final int semicolonOffset = src.get().indexOf(';', src.getLineOffset(endingLine));
-                        if (foundSemicolon == false && semicolonOffset >= 0) {
+                        if (parentIsSimpleDeclaration && semicolonOffset >= 0 && input.getSource().getLineOfOffset(semicolonOffset) >= endingLine + 2) {
                             suggestions.add(new Suggestion(
                                     input.getFilePath(),
                                     src.getLineOfOffset(semicolonOffset),
@@ -93,7 +78,7 @@ public class SemicolonOblivionSuggester extends Suggester {
                                     semicolonOffset, 1,
                                     "構造体の宣言のセミコロンは、最後の閉じ括弧 } の直後に書くと見やすくなります。"
                                     ));
-                        } else if (foundSemicolon == false) {
+                        } else if (parentIsSimpleDeclaration && semicolonOffset == -1) {
                             suggestions.add(new Suggestion(
                                     input.getFilePath(),
                                     declSpec.getFileLocation().getEndingLineNumber() - 1,
